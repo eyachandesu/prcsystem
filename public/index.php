fa893676ab20a53a705ac2f8b637743aed0ccfbc
Mickyl Mickey <mickylmickey12@gmail.com>
2026-03-04 20:52:49 +0800

    merge conflict

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="./dist/output.css" rel="stylesheet">
+  <link href="./public/dist/output.css" rel="stylesheet">
 </head>
 <body>
  <div class="bg-blue-500 text-white p-4">Welcome to the PRCS System</div>
